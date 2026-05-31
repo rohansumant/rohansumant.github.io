65a2e3377af1a8149286ea604cb93aacb0bc64d7
Rohan Sumant <r.s.sumant@gmail.com>
2026-05-30 20:45:39 -0700

    1. Adding some backend support 2. Revised package.json scripts

--- github-pages/app/page.tsx
+++ github-pages/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
-
-export default function Home() {
+export default async function Home() {
+  const response = await fetch('https://helloworld-280029092205.us-west1.run.app/api/time');
+  const data = await response.json();
   return (
     <main>
-      Getting started with Github Pages!
+      Getting started with Github Pages! <br />
+      Current time is {data.time}
     </main>
   );
 }
