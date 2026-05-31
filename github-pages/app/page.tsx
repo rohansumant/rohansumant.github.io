e778832d175d53d96465daf0c6f73664d413b596
Rohan Sumant <r.s.sumant@gmail.com>
2026-05-31 12:45:37 -0700

    Moving to client component to ensure time udpates on page refresh

--- github-pages/app/page.tsx
+++ github-pages/app/page.tsx
@@ -1,10 +1,24 @@
-export default async function Home() {
-  const response = await fetch('https://helloworld-280029092205.us-west1.run.app/api/time');
-  const data = await response.json();
+'use client';
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  
+  const [time, setTime] = useState(null);
+  
+  useEffect(() => {
+    async function fn() {
+      const response = await fetch('https://helloworld-280029092205.us-west1.run.app/api/time');
+      const data = await response.json();
+      setTime(data.time);
+    }
+
+    fn();
+  }, []);
+
   return (
     <main>
       Getting started with Github Pages! <br />
-      Current time is {data.time}
+      Current time is {time ? time : "..."}.
     </main>
   );
 }
